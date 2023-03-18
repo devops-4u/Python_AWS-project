781988b6c451693a59752d861abecf9fe7d09ef5
jatin <jathunnybatra@gmail.com>
2023-03-18 20:56:14 +0530

    Second Commit

--- packages.py
+++ packages.py
@@ -4,4 +4,4 @@ total, free, used= shutil.disk_usage("/")
 print(f"Total disk space: {total // (2**30)}GB")
 print(f"free disk space: {free // (2**30)}GB")
 print(f"Used disk space: {used // (2**30)}GB")
-#print(os.getcwd())
+print(os.getcwd())  #shows current working directory
